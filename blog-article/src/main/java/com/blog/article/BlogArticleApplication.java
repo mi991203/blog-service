a8089bb284d03e0d64e2621d1c41815770450ec9
romanshao <sh1101315853@163.com>
2026-03-21 11:09:28 +0800

    解决启动失败问题：1. mvn配置错误。2. blog-article启动类未设置common模块的component-scan位置

--- blog-article/src/main/java/com/blog/article/BlogArticleApplication.java
+++ blog-article/src/main/java/com/blog/article/BlogArticleApplication.java
@@ -1,9 +1,13 @@
 package com.blog.article;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.blog.article", "com.blog.common"})
+@MapperScan("com.blog.article.mapper")
 public class BlogArticleApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogArticleApplication.class, args);
